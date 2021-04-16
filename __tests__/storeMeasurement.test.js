b310bbc56f648008c00147a3129b00d5115a78d3
Patrick Lee <patricklee1138@gmail.com>
2021-04-16 10:40:48 -0700

    GREEN: store an input number as a measurement

--- __tests__/storeMeasurement.test.js
+++ __tests__/storeMeasurement.test.js
@@ -12,7 +12,23 @@ describe("storeMeasurement", () => {
   test("should store a person's age in (Earth-)years and return it in Earth-years", () => {
     // 
     const yearsEarth = new storeMeasurement(measurement1);
-    expect(storeMeasurement.measurement).toEqual(33);
+    expect(yearsEarth.measurement).toEqual(33);
   });
+
+  // test("should check whether an input measurement is a number", () => {
+  //   // 
+  //   const yearsEarth = new storeMeasurement(measurement1);
+  //   console.log(`
+  //   *********************
+  //   *********************
+
+  //   The argued value is: ${measurement1}
+  //   The stored value is: ${yearsEarth.measurement}
+
+  //   *********************
+  //   *********************
+  //   `)
+  //   expect(yearsEarth.measurement).toEqual(33);
+  // });
 
 });
