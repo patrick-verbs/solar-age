e9832b94ca38ba2bc56988a8b9129ac69059e47c
Patrick Lee <patricklee1138@gmail.com>
2021-04-16 12:14:04 -0700

    WIP: building database of units for time conversions

---
+++ src/js/db_units.js
@@ -0,0 +1,54 @@
+const UNITS = [
+  // TIME UNITS
+  {
+    synonyms: ["ms", "millisecond", "milliseconds"],
+    factor: 1,
+    paradigm: "time",
+  },
+  {
+    synonyms: ["s", "sec", "secs", "second", "seconds"],
+    factor: 1000,
+    paradigm: "time",
+  },
+  {
+    synonyms: ["min", "mins", "minute", "minutes"],
+    factor: 60 * 1000,
+    paradigm: "time",
+  },
+  {
+    synonyms: ["h", "hr", "hrs", "hour", "hours"],
+    factor: 60 * 60 * 1000,
+    paradigm: "time",
+  },
+  {
+    synonyms: ["day", "days"],
+    factor: 24 * 60 * 60 * 1000,
+    paradigm: "time",
+  },
+  {
+    synonyms: ["wk", "week", "weeks"],
+    factor: 7 * 24 * 60 * 60 * 1000,
+    paradigm: "time",
+  },
+  {
+    synonyms: ["yr", "yrs", "year", "years", "Earth-year", "earth-year"],
+    factor: 365 * 24 * 60 * 60 * 1000,
+    paradigm: "time",
+  },
+  {
+    synonyms: ["Gaussian yr", "Gaussian yrs", "Gaussian year", "Gaussian years", "gaussian yr", "gaussian yrs", "gaussian year", "gaussian years", "Earth-year", "Earth year", "earth-year", "earth year", "Earth yr", "earth yr", "avg yr", "avg year", "average yr", "average year"],
+    factor: (365 * 24 * 60 * 60 * 1000) + 22196000,// an additional 6 hours, 9 minutes, 56 seconds
+    paradigm: "time",
+  },
+  // CURRENCY UNITS
+  {
+    synonyms: ["cent", "cents"],
+    factor: 1,
+    paradigm: "currency (United States)",
+  },
+  {
+    synonyms: ["USD", "dollar", "dollars", "$"],
+    factor: 100,
+    paradigm: "currency (United States)",
+  }
+]
